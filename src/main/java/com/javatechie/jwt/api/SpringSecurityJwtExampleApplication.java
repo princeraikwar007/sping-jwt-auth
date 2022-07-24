3e8c0b078f645ada4b81bef1f72c3e3653a37e11
Prince Raikwar <raikwarprince25@gmail.com>
2022-07-24 17:21:23 +0530

    insert data by list

--- src/main/java/com/javatechie/jwt/api/SpringSecurityJwtExampleApplication.java
+++ src/main/java/com/javatechie/jwt/api/SpringSecurityJwtExampleApplication.java
@@ -5,6 +5,7 @@ import com.javatechie.jwt.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -13,19 +14,19 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SpringSecurityJwtExampleApplication {
-//    @Autowired
-//    private UserRepository repository;
-//
-//    @PostConstruct
-//    public void initUsers() {
-//        List<User> users = Stream.of(
-//                new User(101, "javatechie", "password", "javatechie@gmail.com"),
-//                new User(102, "user1", "pwd1", "user1@gmail.com"),
-//                new User(103, "user2", "pwd2", "user2@gmail.com"),
-//                new User(104, "user3", "pwd3", "user3@gmail.com")
-//        ).collect(Collectors.toList());
-//        repository.saveAll(users);
-//    }
+    @Autowired
+    private UserRepository repository;
+
+    @PostConstruct
+    public void initUsers() {
+        List<User> users = Stream.of(
+                new User(101, "javatechie", "password", "javatechie@gmail.com"),
+                new User(102, "user1", "pwd1", "user1@gmail.com"),
+                new User(103, "user2", "pwd2", "user2@gmail.com"),
+                new User(104, "user3", "pwd3", "user3@gmail.com")
+        ).collect(Collectors.toList());
+        repository.saveAll(users);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityJwtExampleApplication.class, args);
